32015df25901046ce304dde63043e8b391bb3cdd
Ezkillz <zzekegomez09@gmail.com>
2019-03-26 15:33:42 -0500

    transition squares almost finished

---
+++ myporfolio/java_script/ticTacToe.js
@@ -0,0 +1,13 @@
+//.1.set up game board
+//. Clear all X's and O's from the board
+//.Decide who makes first move. 
+//
+//.2 PLayer clicks a spot on the board, mark the board if it is unoccupied, else give an error message
+//
+//.3 Check if the game is finished after marking the board
+//. if the game if finished , then ouytputs a message indicating winner
+//. if the game is not finnished then the other player make a move
+//.Step 3 repeats until the game is finished
+//
+//.4 Prompt players to decide of they want to play new game.
+//. If yes, then goes back to step 1.
